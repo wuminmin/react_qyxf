1f98b68281136056d842ac07b276a9f9abdf7f0b
admin <you@example.com>
2019-11-06 16:23:21 +0800

    1

---
+++ src/BraftEditor.js
@@ -0,0 +1,21 @@
+import 'braft-editor/dist/index.css'
+import React from 'react'
+import BraftEditor from 'braft-editor'
+
+export default class PageDemo extends React.Component {
+
+  state = {
+    editorState: BraftEditor.createEditorState(null)
+  }
+
+  render () {
+    return (
+      <BraftEditor value={this.state.editorState} onChange={this.handleChange}/>
+    )
+  }
+
+  handleChange = (editorState) => {
+    this.setState({ editorState })
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import ShouYe from './App'
+import PageDemo from './BraftEditor'
 
 const routing = (
   <Router>
     <div>
       <Route path="/" component={ShouYe} />
+      <Route path="brafteditor/" component={PageDemo} />
     </div>
   </Router>
 )
