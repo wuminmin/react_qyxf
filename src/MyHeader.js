2585e0aa469305c7586590a718a920984e669d2b
admin <you@example.com>
2019-11-23 22:45:18 +0800

    分离header

--- src/MyHeader.js
+++ src/MyHeader.js
@@ -5,7 +5,6 @@ import Qs from 'qs'
 import axios from 'axios'
 import 'antd/dist/antd.css';
 import './index.css';
-import MyHeader from './MyHeader'
 const { SubMenu } = Menu;
 const menu = (
     <Menu>
@@ -156,140 +155,8 @@ const menu7 = (
     </Menu>
 );
 
-class MyMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            菜单列表: [],
-            lan_mu: this.props.lan_mu,
-            ban_kuai: this.props.ban_kuai,
-            myHTML_tittle: '',
-            myHTML_article: '',
-            myHTML_time: '',
-        }
-    }
 
-    componentDidMount() {
-        let self = this;
-        let data = {
-            "type": this.props.lan_mu
-        }
-        axios({
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            method: 'post',
-            url: 'https://wx.wuminmin.top/qyrd/rd_xia_zai_by_lan_mu',
-            data: Qs.stringify(data)
-        }).then(function (response) {
-            console.log(response)
-            self.setState({
-                菜单列表: response.data
-            });
-        })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    handleClick = e => {
-        console.log('click ', e.key);
-        let self = this;
-        let data = {
-            "tittle": e.key
-        }
-        self.setState({
-            myHTML_tittle: e.key
-        });
-        axios({
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            method: 'post',
-            url: 'https://wx.wuminmin.top/qyrd/rd_xia_zai_by_tittle',
-            data: Qs.stringify(data)
-        }).then(function (response) {
-            console.log(response)
-            self.setState({
-                myHTML_article: response.data
-            });
-
-        })
-            .catch(function (error) {
-                console.log(error);
-            });
-        axios({
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            method: 'post',
-            url: 'https://wx.wuminmin.top/qyrd/rd_xia_zai_time_by_tittle',
-            data: Qs.stringify(data)
-        }).then(function (response) {
-            console.log(response)
-            self.setState({
-                myHTML_time: response.data
-            });
-        })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
-
-    render() {
-        return (
-            <Row>
-                <Col span={4}>
-                    <PageHeader
-                        style={{
-                            border: '1px solid rgb(235, 237, 240)',
-                        }}
-                        onBack={() => { window.location = '/' }}
-                        title={this.state.ban_kuai}
-                        subTitle={this.state.lan_mu}
-                    />,
-                    <Menu
-                        onClick={this.handleClick}
-                        style={{ width: 256 }}
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        mode="inline"
-                    >
-                        {
-                            this.state.菜单列表.map((item) => {
-                                return (
-                                    <SubMenu item={item} key={item.月份} title={
-                                        <span>
-                                            <Icon type="setting" />
-                                            <span>{item.月份}</span>
-                                        </span>
-                                    } >
-                                        {
-                                            item.新闻标题列表.map((item2) => {
-                                                return (
-                                                    <Menu.Item key={item2.标题}>{item2.标题}</Menu.Item>
-                                                )
-                                            })
-                                        }
-                                    </SubMenu>
-                                )
-                            })
-                        }
-                    </Menu>
-                </Col>
-                <Col span={2}></Col>
-                <Col span={18}>
-                    <h1 align={'center'}>{this.state.myHTML_tittle}</h1>
-                    <h4 align={'center'}>{this.state.myHTML_time}</h4>
-                    <div dangerouslySetInnerHTML={{ __html: this.state.myHTML_article }} />
-                </Col>
-            </Row>
-
-        )
-    }
-}
-
-export default class MyNews extends React.Component {
+export default class MyHeader extends React.Component {
     state = {
         collapsed: false,
         myHTML: '',
@@ -298,15 +165,6 @@ export default class MyNews extends React.Component {
     };
 
     componentDidMount() {
-        console.log(this.props)
-        const search = this.props.location.search;
-        const params = new URLSearchParams(search);
-        console.log(params.get('ban_kuai'))
-        console.log(params.get('lan_mu'))
-        this.setState({
-            ban_kuai: params.get('ban_kuai'),
-            lan_mu: params.get('lan_mu'),
-        });
     }
 
     onCollapse = collapsed => {
@@ -315,13 +173,57 @@ export default class MyNews extends React.Component {
     };
 
     render() {
-        const search = this.props.location.search;
-        const params = new URLSearchParams(search);
-
         return (
             <div>
-                <MyHeader></MyHeader>
-                <MyMenu ban_kuai={params.get('ban_kuai')} lan_mu={params.get('lan_mu')}></MyMenu>
+                <Carousel>
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=17" alt="" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=18" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=19" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=20" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=21" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=22" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=23" />
+                    <img src="https://wx.wuminmin.top/wxyl/image?id=24" />
+                </Carousel>
+                <Row>
+                    <Col span={3}>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu} placement="bottomCenter">
+                            <Button>首页</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu2} placement="bottomCenter">
+                            <Button>人大概括</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu3} placement="bottomCenter">
+                            <Button>新闻中心</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu4} placement="bottomCenter">
+                            <Button>依法履职</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu5} placement="bottomCenter">
+                            <Button>代表工作</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu6} placement="bottomCenter">
+                            <Button>府委两院</Button>
+                        </Dropdown>
+                    </Col>
+                    <Col span={3}>
+                        <Dropdown overlay={menu7} placement="bottomCenter">
+                            <Button>乡镇人大</Button>
+                        </Dropdown>
+                    </Col>
+                </Row>
 
             </div>
         );
