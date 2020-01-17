5369c80e1a392847ebd8dce34156aba81efdf6ca
wuminmin <buckwmm@qq.com>
2020-01-17 17:13:22 +0800

    上传文章选择器

--- src/BraftEditor.js
+++ src/BraftEditor.js
@@ -1,25 +1,45 @@
-import 'braft-editor/dist/index.css'
-import React from 'react'
-import BraftEditor from 'braft-editor'
-import axios from 'axios'
-import Qs from 'qs'
-import moment from 'moment'
-import { Layout, Menu, Breadcrumb, Icon, Row, Col, Dropdown, Button, Tag, PageHeader } from 'antd';
+import 'braft-editor/dist/index.css';
+import React from 'react';
+import BraftEditor from 'braft-editor';
+import axios from 'axios';
+import Qs from 'qs';
+import moment from 'moment';
+import AppGlobal from './AppGlobal';
+import { TreeSelect, Menu, Select, Icon, Row, Col, Dropdown, Button, Tag, PageHeader } from 'antd';
+import MyHeader from './MyHeader.js';
 
+const { Option, OptGroup } = Select;
 export default class BasicDemo extends React.Component {
-
   state = {
     editorState: BraftEditor.createEditorState('<p>Hello <b>World!</b></p>'), // 设置编辑器初始内容
     outputHTML: '<p></p>',
     myHTML: '',
     tittle: '',
-    type: '',
+    head_menu_data: [],
+    subname: '',
   }
 
   componentDidMount() {
     this.isLivinig = true
     // 3秒后更改编辑器内容
-    setTimeout(this.setEditorContentAsync, 3000)
+    setTimeout(this.setEditorContentAsync, 3000);
+
+    let self = this;
+    axios({
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'post',
+      url: AppGlobal.url.get_header_menu_list_data,
+    }).then(function (response) {
+      console.log(response)
+      self.setState({
+        head_menu_data: response.data
+      });
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   componentWillUnmount() {
@@ -49,24 +69,57 @@ export default class BasicDemo extends React.Component {
 
   render() {
 
-    const { editorState, outputHTML, myHTML } = this.state
+    const { editorState, myHTML } = this.state
 
     return (
       <div>
+        <MyHeader />
+        <Row>
+          <Col span={2}></Col>
+          <Col span={2}> <label>模块名称:</label></Col>
+          <Col span={5}><Select
+            defaultValue=""
+            style={{ width: '100%' }}
+            onChange={(value) => {
+              console.log(`selected ${value}`);
+              this.setState({ subname: value });
+            }}
+          >
+            {this.state.head_menu_data.map((myitem) => {
+              return (
+                <OptGroup label={myitem.name} >
+                  {myitem.myMenu.map((subitem) => {
+                    return (
+                      <Option value={subitem.subname}>{subitem.subname}</Option>
+                    )
+                  })}
+                </OptGroup>
+              )
+            }
+            )}
+          </Select>
+          </Col>
+          <Col span={3}></Col>
+          <Col span={2}><label>新闻标题:</label></Col>
+          <Col span={8}><input style={{width:'100%'}} type="txt" defaultValue="" onChange={this.handleChangeBanShiRiQi2} /></Col>
+          <Col span={2}></Col>
+        </Row>
         <Row>
           <Col span={2}></Col>
           <Col span={20}>
-            <label>模块名称:</label>
-            <input type="txt" defaultValue="" onChange={this.handleChangeBanShiRiQi} />
-            <label>新闻标题:</label>
-            <input type="txt" defaultValue="" onChange={this.handleChangeBanShiRiQi2} />
             <div className="editor-wrapper">
               <BraftEditor
                 value={editorState}
                 onChange={this.handleChange}
               />
             </div>
-            <Button
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
+          <Button
               type="primary"
               onClick={e => {
                 this.setState({
@@ -74,8 +127,22 @@ export default class BasicDemo extends React.Component {
                 });
               }}>预览文章</Button>
 
-            <h5>预览文章</h5>
-            <div dangerouslySetInnerHTML={{ __html: myHTML }} />
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
+            <div className="editor-wrapper">
+              <h5>预览文章</h5>
+              <div dangerouslySetInnerHTML={{ __html: myHTML }} />
+            </div>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
             <Button
               type="primary"
               onClick={e => {
@@ -83,7 +150,7 @@ export default class BasicDemo extends React.Component {
                 let data = {
                   "article": self.state.outputHTML,
                   "tittle": self.state.tittle,
-                  "type": self.state.type,
+                  "type": self.state.subname,
                   "now": moment().format('YYYY-MM-DD HH:mm:ss')
                 }
                 axios({
@@ -105,12 +172,8 @@ export default class BasicDemo extends React.Component {
               }
               }>上传文章</Button>
           </Col>
-          <Col span={2}></Col>
         </Row>
-
       </div>
     )
-
   }
-
 }
